7f4e944ba05fdb6a399210a0ef5e655e4e72cd3f
Bloodpyre <seregabogotir@mail.ru>
2026-03-25 19:18:13 +0700

    Add Amvera config

--- main.py
+++ main.py
@@ -118,4 +118,6 @@ def get_messages(username: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
