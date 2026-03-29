643d758a30cc3eb10df804b0ca5df606157f74c3
Bloodpyre <seregabogotir@mail.ru>
2026-03-29 20:09:18 +0700

    Changed port

--- main.py
+++ main.py
@@ -132,5 +132,5 @@ def get_messages(username: str):
 # ========== ЗАПУСК ==========
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 80))
     uvicorn.run(app, host="0.0.0.0", port=port)
